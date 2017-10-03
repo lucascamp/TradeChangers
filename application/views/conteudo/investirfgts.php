7a128beb689a8fc5497ad5aefd51bc5193d1a3b4
Samuel1s <32439435+Samuel1s@users.noreply.github.com>
2017-10-03 20:37:10 -0300

    Texto Conteudo
    texto alterado

--- application/views/conteudo/investirfgts.php
+++ application/views/conteudo/investirfgts.php
@@ -78,43 +78,41 @@
           <p class="item-intro text-muted">Tesouro Direto.</p>
           <img class="img-fluid d-block mx-auto" src="<?= base_url(); ?>/img/fgts.png" alt="">
           <p align ="Justify">O Fundo de Garantia por Tempo de Serviço (FGTS) é uma reserva que
-            corresponde a 8% do salário do colaborador, depositado mensalmente pelo
-            empregador em uma conta na Caixa Econômica Federal. Em algumas
-            situações, existem possibilidades de saque de FGTS
-            (http://www.fgts.gov.br/perguntas/trabalhador/pergunta07.asp )
-            Uma das situações mais usuais é em caso de demissão, momento em que a
-            pessoa pode sacar seu FGTS e ainda conta com uma porcentagem de juros
-            sobre o saldo do FGTS, paga pelo empregador, como rescisão de contrato de
-            trabalho. Mesmo em tempos de crise, o desempregado pode ficar em dúvida
-            sobre o que fazer com o dinheiro. Nestes momentos de dúvida é imprescindível
-            pensar em estratégias, evitando fazer gastos desnecessários que podem fazer
-            falta futuramente. Pagar dívidas ou até fazer este dinheiro render: como fazer
-            esta escolha? Já pensou em aplicar o seu FGTS?
-            Existem aplicações financeiras, como Tesouro Direito, que garantem ganhos
-            mais vantajosos que a poupança. A taxa de remuneração do Tesouro Direto
-            acompanha a taxa Selic, que atualmente está em 8.25%
-            (http://carteirarica.com.br/taxa-selic/ - LINKAR O TEXTO DO LEO (DE OLHO
-            NOS JUROS). O que a maioria das pessoas que possuem alguma quantia em
-            dinheiro, como o FGTS, não sabem, é que investir não é um “bicho de 7
-            cabeças”, e com pequenos valores a partir de R$ 100,00, seu dinheiro pode
-            render.
-            A maior dificuldade na realização de aplicações e investimentos é que a
-            população ainda é bastante leiga no assunto. Educação financeira deveria ser
-            grade escolar para desenvolvimento de alunos no ensino médio, mas não é o
-            que acontece. Existe ainda um tabu na população de que aplicações
-            financeiras causam perda de dinheiro. O Tesouro Direto, por exemplo, é a
-            venda de títulos públicos federais para pessoas físicas. Dessa forma, o risco é
-            mínimo já que a chance do governo não pagar seus títulos é muita baixa.
-            Segundo Vera Simões, gerente de registros e autorizações da Comissão de
-            Valores Mobiliários, que fiscaliza mercado de capitais, “Risco zero não existe,
-            mas esses são os investimentos de menor risco”.
-            O melhor destino para o seu FGTS após ficar desempregado inicialmente é
-            sempre quitar dívidas, sendo importante uma negociação prévia com os
-            credores para redução das taxas de juros. Se não tiver dividas, o melhor
-            caminho é investir seu dinheiro em algo mais vantajoso para seu bolso, e assim
-            as melhores opções são as aplicações financeiras. Existem empresas que
-            realizam essas destinações de forma automatizada e sem trabalho para seus
-            clientes, buscando maximizar seus lucros e ganhos e diminuir seus riscos.</p>
+             corresponde a 8% do salário do colaborador, depositado mensalmente pelo
+             empregador em uma conta na Caixa Econômica Federal. Em algumas
+             situações, existem possibilidades de saque de FGTS.</p>
+             <p align ="Justify">Uma das situações mais usuais é em caso de demissão, momento em que a
+             pessoa pode sacar seu FGTS e ainda conta com uma porcentagem de juros
+             sobre o saldo do FGTS, paga pelo empregador, como rescisão de contrato de
+             trabalho. Mesmo em tempos de crise, o desempregado pode ficar em dúvida
+             sobre o que fazer com o dinheiro. Nestes momentos de dúvida é imprescindível
+             pensar em estratégias, evitando fazer gastos desnecessários que podem fazer
+             falta futuramente. Pagar dívidas ou até fazer este dinheiro render: como fazer
+             esta escolha? Já pensou em aplicar o seu FGTS?</p>
+             <p align ="Justify">Existem aplicações financeiras, como Tesouro Direito, que garantem ganhos
+             mais vantajosos que a poupança. A taxa de remuneração do Tesouro Direto
+             acompanha a taxa Selic, que atualmente está em 8.25%.
+             O que a maioria das pessoas que possuem alguma quantia em
+             dinheiro, como o FGTS, não sabem, é que investir não é um “bicho de 7
+             cabeças”, e com pequenos valores a partir de R$ 100,00, seu dinheiro pode
+             render.</p>
+             <p align ="Justify">A maior dificuldade na realização de aplicações e investimentos é que a
+             população ainda é bastante leiga no assunto. Educação financeira deveria ser
+             grade escolar para desenvolvimento de alunos no ensino médio, mas não é o
+             que acontece. Existe ainda um tabu na população de que aplicações
+             financeiras causam perda de dinheiro. O Tesouro Direto, por exemplo, é a
+             venda de títulos públicos federais para pessoas físicas. Dessa forma, o risco é
+             mínimo já que a chance do governo não pagar seus títulos é muita baixa.
+             Segundo Vera Simões, gerente de registros e autorizações da Comissão de
+             Valores Mobiliários, que fiscaliza mercado de capitais, “Risco zero não existe,
+             mas esses são os investimentos de menor risco”.</p>
+             <p align ="Justify">O melhor destino para o seu FGTS após ficar desempregado inicialmente é
+             sempre quitar dívidas, sendo importante uma negociação prévia com os
+             credores para redução das taxas de juros. Se não tiver dividas, o melhor
+             caminho é investir seu dinheiro em algo mais vantajoso para seu bolso, e assim
+             as melhores opções são as aplicações financeiras. Existem empresas que
+             realizam essas destinações de forma automatizada e sem trabalho para seus
+             clientes, buscando maximizar seus lucros e ganhos e diminuir seus riscos.</p>
           </div>
         </div>
       </div>
