dcbc0cb948010de36f4a6abedd80b1f056a35991
Gabriel Francisco <gabrinelson27@gmail.com>
2017-10-02 22:04:37 -0300

    Arrumando a merda 1.0
    Falta muita merda ainda pra arrumar

--- application/models/Leads_model.php
+++ application/models/Leads_model.php
@@ -4,7 +4,7 @@ class Leads_model extends MY_Model {
 
     public function __construct() {
         parent::__construct();
-        $this->setTable('tb_leads');
+        $this->setTable('tbl_lead');
     }
 
 }
