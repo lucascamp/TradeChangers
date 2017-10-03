ec41e1e2634a2a06277831ba5e278b57aea4e038
Gabriel Francisco <gabrinelson27@gmail.com>
2017-10-02 22:42:50 -0300

    Corrigindo CSV

--- gerarcsv.php
+++ gerarcsv.php
@@ -4,15 +4,15 @@ $sql = "SELECT * FROM tbl_lead";
 if ($query = mysqli_query($conexao, $sql)){
 	while ($row = mysqli_fetch_assoc($query)) {
 		$tipo = gerenciarTipo($row['email']);
-		echo "{$row['email']},{$row['nome_completo']},{$row['ipv4']},$tipo,{$row['datetime_cadastro']}";
+		echo "{$row['email']},{$row['nome_completo']},{$row['ipv4']},$tipo,{$row['datetime_cadastro']}<br>";
     }
     mysqli_free_result($query);
 }
 mysqli_close($conexao);
 
 function gerenciarTipo($email){
-	$tipos = ["gmail", "yahoo", "msn", "hotmail", "outlook", "live"];
-	$dominio = preg_match("/\@(.*?)\./", $email);
+	$tipos = ["gmail", "yahoo", "msn", "hotmail", "outlook", "live", "bol", "ig", "oi", "pop", "zipmail"];
+	preg_match("/\@(.*?)\./", $email, $dominio);
 	return in_array($dominio[1], $tipos) ? "B2C" : "B2B";
 }
 ?>
