fd6923e0167be968051b82c35cfcf931eeff2fdd
Gabriel Francisco <gabrinelson27@gmail.com>
2017-10-02 21:13:21 -0300

    sla

--- application/views/index.php
+++ application/views/index.php
@@ -1,32 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php $this->load->helper('url'); ?>
-
 <head>
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>TradeChangers</title>
-
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
     <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
-
-    <link href="css/agency.css" rel="stylesheet">
-
+	<link href="css/agency.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
         <div class="container">
-            <a class="navbar-brand js-scroll-trigger" href="#page-top"><img  class="img-fluid" id='logo' src="img/logo.png" alt=""></a>
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+			<img class="img-fluid" id='logo' src="img/logo.png" alt=""></a>
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
           <i class="fa fa-bars"></i>
